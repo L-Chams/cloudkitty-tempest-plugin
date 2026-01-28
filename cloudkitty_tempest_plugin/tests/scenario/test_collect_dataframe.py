19966f84593015d6e82427bad9aba2db911d5f19
Leonie Chamberlin-Medd <leonie@stackhpc.com>
2026-01-28 16:00:21 +0000

    Just adding for backup

--- cloudkitty_tempest_plugin/tests/scenario/test_collect_dataframe.py
+++ cloudkitty_tempest_plugin/tests/scenario/test_collect_dataframe.py
@@ -1,5 +1,3 @@
-
-
 import logging
 import time
 from tempest import config
@@ -53,22 +51,27 @@ class DataFrameCollectionScenarioTest(base.BaseRatingTest):
 
         # create a volume resource
         self.volume = self.os_admin.vol_client.create_volume(size=2, name='cloudkitty_test_vol')
-        volume_id = self.volume['volume']['id']
-        self.addCleanup(self.os_admin.vol_client.delete_volume, volume_id)
+        self.volume_id = self.volume['volume']['id']
+        self.project_id = self.os_admin.credentials.project_id
+        self.addCleanup(self.os_admin.vol_client.delete_volume, self.volume_id)
         # create a hashmap service for storage
-        self.storage_service = self.rating_client.create_hashmap_service(name='volume.size')
+        self.storage_service = self.rating_client.create_hashmap_service(name='storage')
         self.storage_service_id = self.storage_service['service_id']
         self.addCleanup(self.rating_client.delete_hashmap_service, self.storage_service_id)
+        # create a mapping
+        # set to today's current date and time
+        #start_date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
 
+
+        # enable hashmap module
+        self.rating_client.update_rating_module(module_name='hashmap', enabled=True,  priority=100)
         self.mapping = self.rating_client.create_hashmap_mapping(cost=2,
                                                             service_id=self.storage_service_id)
-        mapping_id = self.mapping['mapping_id']
-        self.addCleanup(self.rating_client.delete_hashmap_mapping, mapping_id)
-
-
+        self.mapping_id = self.mapping['mapping_id']
+        self.addCleanup(self.rating_client.delete_hashmap_mapping, self.mapping_id)
 
         # wait for a specified amount of time
-        time.sleep(180) #????
+        time.sleep(600) #testing 10 minutes
 
     # collect the dataframe and verify its correctness
     def _get_dataframe(self):
@@ -90,24 +93,37 @@ class DataFrameCollectionScenarioTest(base.BaseRatingTest):
             self.fail(f"No dataframes were collected. Received: {self.dataframes}")
 
 
-        print("Collected DataFrames:", dataframes_list)
+        # loop through dataframes and look for the one that matches self.volume id
 
-        first_df = dataframes_list[0]['resources'][0]
+        for df in dataframes_list:
+            if df['desc']['id'] == self.volume['volume']['id']:
+                test_df = df
+                break
+        else:
+            LOG.error(f"No matching dataframe found for volume ID {self.volume['volume']['id']}. Dataframes: {dataframes_list}")
+            self.fail(f"No matching dataframe found for volume ID {self.volume['volume']['id']}.")
 
-        with open('/home/ubuntu/out.txt', 'a') as f:
-            print("first dataframe is: ", file=f)
-            print(first_df, file=f)
-            print("self volume id is: ", file=f)
-            print(self.volume['volume']['id'], file=f)
-            print ("self volume project id is: ", file=f)
-            print(self.volume['volume']['project_id'], file=f)
-            print("self volume user id is: ", file=f)
-            print(self.volume['volume']['user_id'], file=f)
 
-        self.assertEqual(first_df['service'], 'volume.size') #works
-        self.assertEqual(first_df['desc']['id'], self.volume['volume']['id'])
-        self.assertEqual(first_df['desc']['project_id'], self.volume['volume']['project_id'])
-        self.assertEqual(first_df['desc']['user_id'], self.volume['volume']['user_id'])
+        #last_df = dataframes_list[-1]['resources'][0]
+
+        with open('/home/ubuntu/dataframe_info_7.txt', 'w') as f:
+            #first print desired outputs
+            print("self.volume id:", self.volume['volume']['id'], file=f)
+            print("self.volume project_id:", self.project_id, file=f)
+            print("self.volume user_id:", self.volume['volume']['user_id'], file=f)
+
+            for df in dataframes_list:
+                print("DataFrame:", df, file=f)
+                for resource in df.get('resources', []):
+                    print("Resource:", resource, file=f)
+
+
+
+
+        self.assertEqual(test_df['service'], 'storage') #works
+        self.assertEqual(test_df['desc']['id'], self.volume['volume']['id'])
+        self.assertEqual(test_df['desc']['project_id'], self.project_id)
+        self.assertEqual(test_df['desc']['user_id'], self.volume['volume']['user_id'])
 
         #check rating has a non-zero value
-        self.assertGreater(float(first_df['rating']), 0.0)
+        self.assertGreater(float(test_df['rating']), 0.0)
